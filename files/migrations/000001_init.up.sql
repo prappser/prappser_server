31dbd32d10e873c1b3a025d95f24233228ac97e6
Crisperit <krzysztof.perchlicki@gmail.com>
2026-01-15 13:32:15 +0100

    fix: use BYTEA instead of BLOB for PostgreSQL

--- files/migrations/000001_init.up.sql
+++ files/migrations/000001_init.up.sql
@@ -104,10 +104,10 @@ CREATE TABLE setup_config (
 -- Server keys table (Ed25519 keypair encrypted with master password)
 CREATE TABLE server_keys (
     id TEXT PRIMARY KEY DEFAULT 'main',
-    public_key BLOB NOT NULL,
-    encrypted_private_key BLOB NOT NULL,
-    salt BLOB NOT NULL,
-    nonce BLOB NOT NULL,
+    public_key BYTEA NOT NULL,
+    encrypted_private_key BYTEA NOT NULL,
+    salt BYTEA NOT NULL,
+    nonce BYTEA NOT NULL,
     created_at BIGINT NOT NULL,
     algorithm TEXT NOT NULL DEFAULT 'ed25519'
 );
